bf5c4fb94aff5fd54ce0822468aae70bf0c05e80
Dayone-6 <82435692+Dayone-6@users.noreply.github.com>
2024-03-22 00:58:35 +1000

    Delete LifestyleHUB directory

--- LifestyleHUB/app/src/main/java/ru/dayone/lifestylehub/utils/FailureCode.kt
+++
@@ -1,6 +0,0 @@
-package ru.dayone.lifestylehub.utils
-
-enum class FailureCode {
-    DEFAULT,
-    GET_WEATHER_FAILED
-}
--- LifestyleHUB/app/src/main/java/ru/dayone/lifestylehub/utils/SuccessCode.kt
+++
@@ -1,6 +0,0 @@
-package ru.dayone.lifestylehub.utils
-
-enum class SuccessCode {
-    DEFAULT,
-    GET_WEATHER_SUCCEED
-}
